8c04c46ff786c6730e8ce4dff47cbb1149017507
BradOFarrell <bradofarrell@gmail.com>
2017-11-23 01:49:27 -0500

    integer skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -6,12 +6,19 @@ puts ($a or $b)
 
 #Challenge 2: Nil
 $nilvar = NIL
-p $nilvar
-puts $nilvar
-print $nilvar
+p ($nilvar)
+puts ($nilvar)
+print ($nilvar)
 puts "Value of var is #{$nilvar}."
 
 #Challenge 3: Integers
+$d = 2
+puts ($d)
+$e = 5.2
+puts ($e)
+$d = $e
+puts ($d)
+
 #Challenge 4: Strings
 #Challenge 5: Input and Output
 #Challenge 6: Arrays
