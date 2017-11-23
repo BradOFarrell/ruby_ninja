cba7f459be310c54c6acb4f90b6c739b191ce70f
BradOFarrell <bradofarrell@gmail.com>
2017-11-23 01:23:31 -0500

    Boolean skills

---
+++ ninja_skills.rb
@@ -0,0 +1,15 @@
+#Challenge 1: Booleans
+$a = true
+$b = false
+puts ($a and $b)
+puts ($a or $b)
+
+#Challenge 2: Nil
+#Challenge 3: Integers
+#Challenge 4: Strings
+#Challenge 5: Input and Output
+#Challenge 6: Arrays
+#Challenge 7: Hashes
+#Challenge 8: Ranges
+#Challenge 9: Sum of Natural Numbers
+#The Final Challenge: Prime Numbers
