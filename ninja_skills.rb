f88bc086fdb71ab1e49940cfbbbe07a89742138f
BradOFarrell <bradofarrell@gmail.com>
2017-11-23 02:13:16 -0500

    array skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -1,3 +1,4 @@
+=begin
 #Challenge 1: Booleans
 $a = true
 $b = false
@@ -33,8 +34,19 @@ $name = gets.chomp
 puts 'What do you like to do?'
 $hobby = gets.chomp
 puts ("My name is "+$name+" and I like to "+$hobby)
+=end
 
 #Challenge 6: Arrays
+$myFirstArray = []
+$myFirstArray = ["1","2","3","4","5",6,7,8,9]
+puts ($myFirstArray.last)
+puts ($myFirstArray.first)
+$myFirstArray[2] = "Joe"
+$myFirstArray[3] = 3
+$myFirstArray.push(true)
+puts($myFirstArray[4].class.name)
+
+
 #Challenge 7: Hashes
 #Challenge 8: Ranges
 #Challenge 9: Sum of Natural Numbers
